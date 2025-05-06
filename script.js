15f86a6de85501a5d32b7e52186103465ad92b0d
h0ki4 <y.fix123456789@gmail.com>
2025-05-06 22:17:48 +0200

    add eye's

---
+++ script.js
@@ -0,0 +1,26 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   event.preventDefault();
+//   const input = document.querySelector("input");
+//   event.preventDefault();
+//   if (input.type === "password") {
+//     input.type = "text";
+//   }
+// });
+document.addEventListener("DOMContentLoaded", () => {
+  const visibilityButtons = document.querySelectorAll(".btn_visability");
+
+  visibilityButtons.forEach((button) => {
+    const input = button.previousElementSibling; // поле ввода перед кнопкой
+    const icon = button.querySelector("i");
+
+    button.addEventListener("click", () => {
+      if (input.type === "password") {
+        input.type = "text";
+        icon.classList.replace("fa-eye", "fa-eye-slash");
+      } else {
+        input.type = "password";
+        icon.classList.replace("fa-eye-slash", "fa-eye");
+      }
+    });
+  });
+});

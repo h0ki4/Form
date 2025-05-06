d0fc64e2d34c0797a0753335e58b00d6c06b5941
h0ki4 <y.fix123456789@gmail.com>
2025-05-06 22:32:25 +0200

    close eye for first

--- script.js
+++ script.js
@@ -10,16 +10,16 @@ document.addEventListener("DOMContentLoaded", () => {
   const visibilityButtons = document.querySelectorAll(".btn_visability");
 
   visibilityButtons.forEach((button) => {
-    const input = button.previousElementSibling; // поле ввода перед кнопкой
+    const input = button.previousElementSibling;
     const icon = button.querySelector("i");
 
     button.addEventListener("click", () => {
       if (input.type === "password") {
         input.type = "text";
-        icon.classList.replace("fa-eye", "fa-eye-slash");
+        icon.classList.replace("fa-eye-slash", "fa-eye");
       } else {
         input.type = "password";
-        icon.classList.replace("fa-eye-slash", "fa-eye");
+        icon.classList.replace("fa-eye", "fa-eye-slash");
       }
     });
   });
